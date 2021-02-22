13e501f12ef3971a4d35a58bc4e6aff710f6ed99
Rafael Moreci <79222289+Terexes@users.noreply.github.com>
2021-02-22 19:28:21 -0300

    Update caesar_cipher.py
    Added code refactoring and some user improvements.

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,51 +2,33 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def encode(text, shift):
-    msg = text
-    msg = list(msg)
-    aux = 0
-    encoded_msg = []
-    for i in msg:
-        if i == " ":
-            encoded_msg.append(i)
-            continue
-        aux = alphabet.index(i) + shift
-        if aux > 25:
-            aux -= 26  # 26 is the total letters of alphabet
-        aux = alphabet[aux]
-        encoded_msg.append(aux)
-    print("Your message is:", "".join(encoded_msg))
+def caesar(text_direction):
+    text = input("Insert your message: \n").lower()
+    shift = int(input("Insert the shift number: \n"))
+    if shift > len(alphabet):
+        shift = shift % 26
+        print(
+            f"Your shift number chosen was too high !\nYour new shift number is: {shift}")
+    encoded_msg = ""
+    if text_direction == "decode":
+        shift *= - 1
+    for char in text:
+        if char in alphabet:  # checks for letters in alphabet
+            position = alphabet.index(char)
+            new_position = position + shift
 
+            if new_position > 25:
+                new_position -= 26
+            encoded_msg += alphabet[new_position]
+        else:  # if its not a letter, pass it directly to encoded_msg
+            encoded_msg += char
 
-def decode(text, shift):
-    msg = text
-    msg = list(msg)
-    aux = 0
-    encoded_msg = []
-    for i in msg:
-        if i == " ":
-            encoded_msg.append(i)
-            continue
-        aux = alphabet.index(i) - shift
-        aux = alphabet[aux]
-        encoded_msg.append(aux)
-    print("Your message is:", "".join(encoded_msg))
+    print(f"Your {text_direction}d message is: {encoded_msg}")
 
 
 while True:
     direction = input(
         "Type 'encode' to encrypt, type 'decode' to decrypt or type 'leave' to leave: \n")
-
-    if direction == "encode":
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        encode(text, shift)
-    elif direction == "decode":
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        decode(text, shift)
-    elif direction == "leave":
+    if direction == "leave":
         break
-    else:
-        print("Please choose a valid option.")
+    caesar(direction)
